81cd7a7d71606b18ed773a88afc323e1d5ba22d1
LaTotty <latotty91@gmail.com>
2025-12-12 10:58:17 +0100

    more tests

--- 2025/go/day10_test.go
+++ 2025/go/day10_test.go
@@ -26,12 +26,22 @@ func TestDay10Example(t *testing.T) {
 			part2Result: "33",
 		},
 		{
-			name: "small",
-			input: `
-				[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
-				`,
+			name:        "small1",
+			input:       `[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}`,
 			part1Result: "2",
 			part2Result: "10",
+		},
+		{
+			name:        "small2",
+			input:       `[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}`,
+			part1Result: "3",
+			part2Result: "12",
+		},
+		{
+			name:        "small3",
+			input:       `[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}`,
+			part1Result: "2",
+			part2Result: "11",
 		},
 	}
 
